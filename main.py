e921231a4c5c90baa0897edc1de8f16721f9bb2a
kennyjacobson <kenny.jacobson@gmail.com>
2023-02-16 20:53:55 -0800

    ready for students

--- main.py
+++ main.py
@@ -2,30 +2,8 @@ from constraint import Problem
 
 
 def coloring_map():
-  p = Problem()
-  p.addVariable('WA', ["Red", "Green", "Blue"])
-  p.addVariable('NT', ["Red", "Green", "Blue"])
-  p.addVariable('SA', ["Red", "Green", "Blue"])
-  p.addVariable('Q', ["Red", "Green", "Blue"])
-  p.addVariable('NSW', ["Red", "Green", "Blue"])
-  p.addVariable('V', ["Red", "Green", "Blue"])
-  p.addVariable('T', ["Red", "Green", "Blue"])
-
-  sol1 = p.getSolution()
-  print(sol1)
-
-  p.addConstraint(lambda a, b: a != b, ("WA", "SA"))
-  p.addConstraint(lambda a, b: a != b, ("NT", "SA"))
-  p.addConstraint(lambda a, b: a != b, ("Q", "SA"))
-  p.addConstraint(lambda a, b: a != b, ("NSW", "SA"))
-  p.addConstraint(lambda a, b: a != b, ("V", "SA"))
-  p.addConstraint(lambda a, b: a != b, ("WA", "NT"))
-  p.addConstraint(lambda a, b: a != b, ("NT", "Q"))
-  p.addConstraint(lambda a, b: a != b, ("Q", "NSW"))
-  p.addConstraint(lambda a, b: a != b, ("NSW", "V"))
-
-  sol2 = p.getSolution()
-  print(sol2)
+    # Part 1 - we will do this together in class.
+    pass
 
 
 def scheduling_problem():
